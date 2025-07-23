a2a6b1c41c952e0f813d36179c81b59a8cbae1c1
BooRuleDie <booruledie@gmail.com>
2025-07-23 11:59:08 +0300

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://dev.booruledie.com',
+      'https://booruledie.com',
+    ],
+  });
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`App started at port ${port}`);
